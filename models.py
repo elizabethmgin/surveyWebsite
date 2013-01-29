ee33fa8a52de74a83b5881971a6a2613880fe04a
anxiaostudio <an@anxiaostudio.com>
2013-01-28 16:52:29 -0800

    Created an initial data model Changed "last name" and "first name" to "surname" and "given name", as family names sometimes are the first name. More important for the user than for us, but good to be consistent Having trouble getting the three boxes to appear side by side

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+"""
+Commented out Mongo stuff for now while we figure out the server situation.
+from flaskext.mongoalchemy import MongoAlchemy
+
+app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'x'
+app.config['MONGOALCHEMY_USER'] = 'x'
+app.config['MONGOALCHEMY_PASSWORD'] = 'x'
+app.config['MONGOALCHEMY_SERVER_AUTH'] = False
+db = MongoAlchemy(app)
+"""
+
+#This assumes we're using shelve but should work for anything
+class User(object):
+    def __init__(self, given_name=None, surname=None, home=None, age=None, gender=None):
+        self.given_name = given_name
+        self.surname = surname
+        self.full_name = str(given_name) + " " + str(surname)
+        self.home = home
+        self.age = age
+        self.gender = gender
